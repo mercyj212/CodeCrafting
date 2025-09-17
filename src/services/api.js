dc1b8397b8f8d2a45a38b233adc8ac3cea0e5e2f
mercyj212 <jaymercy510@gmail.com>
2025-09-17 14:48:34 +0100

    Api base URL updated

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { auth } from './auth';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://codecrafting-api.onrender.com/api/v1',
     timeout: 10000
 });
 
